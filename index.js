354257826005cc1a2af47807a73a7c74b2f90322
Bill Hamilton <bill.hamilton@mulesoft.com>
2022-06-27 22:18:24 -0400

    added class picker logic

--- index.js
+++ index.js
@@ -122,13 +122,26 @@ app.get('/character/create2', (req, res) => {
             { "Chrasima"    : chr} 
                ] 
     }
+    var goodClass = suggestClass(character);
+    var goodCharacter = {
+        Name: name,
+        Stats: [ 
+            { "Strength"    : str},
+            { "Dexterity"   : dex}, 
+            { "Constitution": con}, 
+            { "Intelligence": int}, 
+            { "Wisdoem"     : wis}, 
+            { "Chrasima"    : chr} 
+               ],
+        Class: goodClass 
+    }
 
     // Assuming everything is good, return the 200 all good status code
     res.status(200);
     // Tell the client what data format our return data is in
     res.append('Content-Type', 'application/json');
     // Return the JSON structure as a string
-    res.send(JSON.stringify(character)); 
+    res.send(JSON.stringify(goodCharacter)); 
 });
 
 // GET /
@@ -161,3 +174,47 @@ console.log( process.env.PORT );
 // This is the final call that doesn't return
 // It starts up the REST Service
 app.listen(process.env.PORT, () => console.log("Listening for requests."));
+
+function suggestClass( pc ) {
+    var ClericScore = 0;
+    var FighterScore = 0;
+    var MagicUserScore = 0;
+    var ThiefScore = 0;
+    var DwarfScore = 0;
+    var ElfScore = 0;
+    var HalflingScore = 0;
+    var finalClass = "fighter";
+    
+    //Prime Requisite Matters
+    if (pc.Stats[0].Strength > 13 ) { FighterScore = FighterScore + 10; ElfScore = ElfScore + 5; HalflingScore = HalflingScore + 5 }
+    if (pc.Stats[1].Dexterity > 13 ) { ThiefScore = ThiefScore + 10; HalflingScore = HalflingScore + 5 }
+    if (pc.Stats[2].Constitution > 13 ) { DwarfScore = DwarfScore + 10 }
+    if (pc.Stats[3].Intelligence > 13 ) { MagicUserScore = MagicUserScore + 10; ElfScore = ElfScore + 5 }
+    if (pc.Stats[4].Wisdom > 13 ) { ClericScore = ClericScore + 10 }
+
+    //assume 'fighter'
+    finalClass = "fighter";
+    var bestScore = 0;
+    //calc fighter score
+    FighterScore = FighterScore + (pc.Stats[0].Strength - 10 );
+    bestScore = FighterScore;
+    //calc magic-user score
+    MagicUserScore = MagicUserScore + (pc.Stats[3].Intelligence - 10);
+    if (MagicUserScore >= FighterScore ) { finalClass = "magic-user"; bestScore = MagicUserScore }
+    //calc cleric score
+    ClericScore = ClericScore + (pc.Stats[4].Wisdom - 10);
+    if (ClericScore >= bestScore) { finalClass = "cleric"; bestScore = ClericScore}
+    //calc thief score
+    ThiefScore = ThiefScore + (pc.Stats[1].Dexterity - 10);
+    if (ThiefScore >= bestScore ) { finalClass = "thief"; bestScore = ThiefScore}
+    //calc dwarf
+    DwarfScore = DwarfScore + (pc.Stats[2].Constitution - 10);
+    if (DwarfScore >= bestScore ) { finalClass = "dwarf"; bestScore = DwarfScore}
+    //calc elf
+    ElfScore = ElfScore + (pc.Stats[0].Strength - 10 ) + (pc.Stats[3].Intelligence - 10);
+    if (ElfScore >= bestScore) {finalClass = "elf"; bestScore = ElfScore}
+    //calc halfling
+    HalflingScore = HalflingScore + (pc.Stats[0].Strength - 10 ) + (pc.Stats[1].Dexterity - 10)
+    if (HalflingScore >= bestScore ) {finalClass = "halfling"; bestScore = HalflingScore }
+    return finalClass;
+}
